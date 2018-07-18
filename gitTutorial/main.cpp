a80fd9b5ff1d3e6bb36ba2b600d4ef35349a4503
Gio <giovanni.barranco12@gmail.com>
2018-07-18 10:55:03 -0700

    Initial Commit

---
+++ gitTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitTutorial
+//
+//  Created by Giovanni Barranco on 7/18/18.
+//  Copyright © 2018 Giovanni Barranco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
